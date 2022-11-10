c1cb304eac7a433cc8d339a700567f4003eb6109
Yuriy Zhiganov <115638863+Juner499@users.noreply.github.com>
2022-11-10 08:47:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,22 +12,22 @@
 let links = document.links;
 let btnK = document.getElementsByName("btnK")[0];
 let keywords = ["10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress", "Вывод произвольных типов записей и полей в WordPress"];
-let keyword = keywords[getRandom(0, keywords.lenght)];
+let keyword = keywords[getRandom(0, keywords.length)];
 
 if (btnK !== undefined) {
   document.getElementsByName("q")[0].value = keyword;
   btnK.click();
 } else {
-for (let i = 0; i < links.length; i++) {
+  for(let i = 0; i < links.length; i++) {
     if (links[i].href.indexOf("napli.ru") !== -1) {
-    let link = links[i];
-        console.log("Нашел строку " + link);
-        link.click();
-        break;
+      let link = links[i];
+      console.log("Нашел строку " + link);
+      link.click();
+      break;
     }
   }
 }
 
 function getRandom(min, max) {
-return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
