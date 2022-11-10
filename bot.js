4bb907a7f2ccfb4dfb025055c4fe1f21faccba81
Yuriy Zhiganov <115638863+Juner499@users.noreply.github.com>
2022-11-10 16:07:47 +0300

    Задание №1

--- bot.js
+++ bot.js
@@ -1,31 +1,36 @@
 // ==UserScript==
-// @name         Google Bot
+// @name         Bing Bot
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Bot for Google
+// @description  Bot for Bing
 // @author       Yuriy
-// @match        https://www.google.com/*
+// @match        https://www.bing.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
+
 let links = document.links;
-let btnK = document.getElementsByName("btnK")[0];
+let search_icon = document.getElementById("search_icon");
 let keywords = ["10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress", "Вывод произвольных типов записей и полей в WordPress"];
 let keyword = keywords[getRandom(0, keywords.length)];
 
-if (btnK !== undefined) {
-  document.getElementsByName("q")[0].value = keyword;
-  btnK.click();
+if (search_icon !== null) {
+  document.getElementById("sb_form_q").value = keyword;
+  search_icon.click();
+
 } else {
   for(let i = 0; i < links.length; i++) {
     if (links[i].href.indexOf("napli.ru") !== -1) {
       let link = links[i];
+
       console.log("Нашел строку " + link);
       link.click();
       break;
     }
+
   }
+
 }
 
 function getRandom(min, max) {
